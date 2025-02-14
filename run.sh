792c35cfbf7017257a2f4006c77b1f39fccb54d5
FedjaW <f.wilms5189@gmail.com>
2025-02-14 22:37:55 +0100

    dry run by flag --dry

--- run.sh
+++ run.sh
@@ -4,8 +4,44 @@
 # even if the script is executed from another directory
 script_dir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
-# first argument passed to the script
-filter=$1
+# arguments passed to the script
+filter=""
+dry="0"
+
+# $# is the number of arguments
+while [[ $# > 0 ]]; do
+    if [[ $1 == "--dry" ]] ; then
+        dry="1"
+    else 
+        filter=$1
+    fi
+    # remove the first argument from the arguments list
+    # so to say: pop of every argmument as the while loop loops
+    shift
+done
+
+log() {
+    # $@ means print out everything that was passed
+    # as an argument to the function log
+    if [[ $dry == "1" ]] ; then
+        echo "[DRY_RUN]: $@"
+    else 
+        echo "$@"
+    fi
+}
+
+execute() {
+    log "execute $@"
+    if [[ $dry == "1" ]] ; then
+        return
+    fi
+
+    # execute everything
+    "$@"
+}
+
+
+log "$script_dir -- $filter"
 
 # start a new process and cd's into the dir
 cd $script_dir
@@ -18,8 +54,8 @@ scripts=$(find ./runs -maxdepth 1 -mindepth 1 -type f -perm +100)
 for script in $scripts; do
     # -q: quit, -v: inverse (not contains)
     if echo "$script" | grep -qv "$filter"; then
-        echo "skipping '$script' due to filter '$filter'"
+        log "skipping '$script' due to filter '$filter'"
         continue
     fi
-    ./$script
+    execute ./$script
 done
